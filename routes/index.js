54208478017ef58a697ee1a6fb5f95e8c179b1d4
Nicolay <Nicolay.JG@outlook.com>
2017-05-09 11:31:49 -0500

    se cambia el nombre del aplicativo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My App' });
+  res.render('index', { title: 'Aplicaneitor' });
 });
 
 module.exports = router;
